cad066edfa13f22687ce4d35616be1dbe055b54a
Samat <samat.alpyssov@gmail.com>
2020-01-23 18:17:23 +0600

    Added to React app

---
+++ my-app/src/components/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes.js"
+
+function App() {
+    return (
+        <div> 
+            <Header/>
+            {notes.map(element => (
+                <Note 
+                    key = {element.key}
+                    title = {element.title}
+                    content = {element.content}
+                />
+            ))}
+            {/* {notes.map(Note)} */}
+            <Footer/>
+        </div>
+    );
+}
+
+export default App;
